92517d485961e32b0323e290d65f20dfbff2bffd
Aryan Das <122392506+aryandas2911@users.noreply.github.com>
2025-01-29 13:10:03 +0530

    Updated Website

---
+++ script.js
@@ -0,0 +1,7 @@
+function registerVolunteer() {
+  alert("Volunteer registration is coming soon! Stay tuned.");
+}
+
+function bookAmbulance() {
+  alert("Ambulance booking feature will be available soon!");
+}
